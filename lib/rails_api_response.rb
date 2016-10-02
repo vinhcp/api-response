61647766f0f285da09081011c56be563e726c9f2
cpvinh <cpvinh@gmail.com>
2016-10-02 14:58:36 +0700

    Fix core

--- lib/rails_api_response.rb
+++ lib/rails_api_response.rb
@@ -1,10 +1,11 @@
 require "rails_api_response/version"
 
 module RailsApiResponse
-  module Core
-    module ControllerHelpers
-      autoload :ResponseTemplate, 'core/controller_helpers/response_template'
-      autoload :Responses, 'core/controller_helpers/responses'
-    end
+  module ControllerHelpers
+    autoload :ResponseTemplate, 'core/controller_helpers/response_template'
+    autoload :Responses, 'core/controller_helpers/responses'
   end
+end
+
+module RailsApiResponse
 end
