121dbe7f8e3864d8569d34d3093e21d9ecd37391
cpvinh <cpvinh@gmail.com>
2016-10-02 15:00:52 +0700

    Fix wrong path

--- lib/rails_api_response.rb
+++ lib/rails_api_response.rb
@@ -2,10 +2,7 @@ require "rails_api_response/version"
 
 module RailsApiResponse
   module ControllerHelpers
-    autoload :ResponseTemplate, 'core/controller_helpers/response_template'
-    autoload :Responses, 'core/controller_helpers/responses'
+    autoload :ResponseTemplate, 'rails_api_response/controller_helpers/response_template'
+    autoload :Responses, 'rails_api_response/controller_helpers/responses'
   end
-end
-
-module RailsApiResponse
 end
